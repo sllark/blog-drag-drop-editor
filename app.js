e959df14e68cb50767c3c8b731fb996c2c4d99db
Jack <abdulrehman.6ab@gmail.com>
2021-09-22 17:25:10 +0500

    updated api url

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(postRoutes);
 
 app.use('/', (req, res, next) => {
 
-    res.json({"message": "success 1"});
+    res.json({"message": "welcome to Blog App API."});
 
 })
 
--- client/src/assets/config/configs.js
+++ client/src/assets/config/configs.js
@@ -1,5 +1,5 @@
 const configs = {
-    api_url: process.env.REACT_APP_VERCEL_ENV ? "https://blog-app-node.vercel.com" : "http://localhost:5000"
+    api_url: process.env.REACT_APP_VERCEL_ENV ? "https://blog-app-node.vercel.app" : "http://localhost:5000"
 }
 
 export default configs
