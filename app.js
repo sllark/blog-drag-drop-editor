d1b660515f27bbfbdf5222e1ac0cf92631895a04
Jack <abdulrehman.6ab@gmail.com>
2021-09-22 13:52:42 +0500

    updated env varibales

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const postRoutes = require('./routes/postRoutes');
 
 const app = express();
 
-const MONGOURI = 'mongodb://127.0.0.1:27017/blog?gssapiServiceName=mongodb';
+const {MOGOURI} = require('./config/keys')
+const PORT = process.env.PORT || 5000;
 
 
 app.use(bodyParser.json());
-
 
 
 
@@ -66,15 +66,15 @@ app.use((error, req, res, next) => {
 
 
 
-mongoose.connect(MONGOURI,
+mongoose.connect(MOGOURI,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
     .then((reponse) => {
 
-        console.log('connected!')
-        app.listen(5000);
+        app.listen(PORT);
+        console.log(`connected! at post ${PORT}`)
 
 
     })
